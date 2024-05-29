af067bfbaede20ea2f19ec7175d65f5285791a1d
Hari Prediansyah <coyhari9@gmail.com>
2024-05-29 10:08:44 +0700

    add new

--- usetailwind/tailwind.config.js
+++ usetailwind/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': "#06e303"
+      }
+    },
   },
   plugins: [],
 }
